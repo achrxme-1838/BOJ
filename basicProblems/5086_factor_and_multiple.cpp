159f8368c29b95af8179b1e37a0acea26a440fe5
achrxme <lsh1838@naver.com>
2023-08-10 13:49:54 +0900

    create repository

---
+++ basicProblems/5086_factor_and_multiple.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+class FactorAndMultiple
+{
+private:
+    int input_1;
+    int input_2;
+
+public:
+    FactorAndMultiple(const int input_1, const int input_2)
+        : input_1(input_1), input_2(input_2)
+    {}
+
+
+
+};
+
+
+int main()
+{
+
+    return 0;
+}
