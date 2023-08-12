917902369f6be9b64406d3b006fd20a2bf226472
achrxme <lsh1838@naver.com>
2023-08-12 19:47:47 +0900

    194532 solved

---
+++ basicProblems/19532_math_is_remote_class.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e, f;
+    cin >> a >> b >> c >> d >> e >> f;
+
+    int ans_x, ans_y;
+    bool sol_index = false;
+
+    for(int x = -999; x <= 999; x++)
+    {
+        for(int y = -999; y <= 999; y++)
+        {
+            if(a*x + b*y == c and d*x + e*y == f)
+            {
+                ans_x = x;
+                ans_y = y;
+                sol_index = true;
+                break;
+            }
+        }
+
+        if(sol_index) break;
+    }
+
+    cout << ans_x << " " << ans_y;
+
+
+    return 0;
+}
